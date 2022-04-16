263fca92b07ff2b4de703cf93c88ba8d2f9d0d43
CristianCharo19 <80004163+CristianCharo19@users.noreply.github.com>
2022-04-16 17:26:24 -0500

    Implementación editar articulo

--- administrativo/controlador.php
+++ administrativo/controlador.php
@@ -1,8 +1,4 @@
 <?php
-    print_r($_POST);
-    ?>
-
-<?php
 require '../vendor/autoload.php';
 
 $pelicula = new Kawschool\Articulo;
@@ -41,6 +37,59 @@ if($_SERVER['REQUEST_METHOD'] ==='POST'){
             print 'Error al registrar un articulo';
     }
 }
+
+if ($_POST['accion']==='Editar'){
+
+        if(empty($_POST['articulo']))
+        exit('Completar articulo');
+    
+    if(empty($_POST['descripcion']))
+        exit('Completar descripcion');
+
+    if(empty($_POST['categoriaid']))
+        exit('Seleccionar una Categoria');
+
+    if(!is_numeric($_POST['categoriaid']))
+        exit('Seleccionar una Categoria válida');
+
+    
+    $_params = array(
+        'articulo'=>$_POST['articulo'],
+        'descripcion'=>$_POST['descripcion'],
+        'precio'=>$_POST['precio'],
+        'categoriaid'=>$_POST['categoriaid'],
+        'fecha'=> date('Y-m-d'),
+        'id'=>$_POST['id'],
+    );
+
+    if(!empty($_POST['imagen_temp']))
+        $_params['imagen'] = $_POST['imagen_temp'];
+    
+    if(!empty($_FILES['imagen']['name']))
+        $_params['imagen'] = subirimagen();
+
+    $rpt = $pelicula->actualizar($_params);
+    if($rpt)
+        header('Location: articulos/index.php');
+    else
+        print 'Error al actualizar un articulo';
+
+    }
+
+}
+
+if($_SERVER['REQUEST_METHOD'] ==='GET'){
+
+    $id = $_GET['id'];
+
+    $rpt = $pelicula->eliminar($id);
+    
+    if($rpt)
+        header('Location: articulos/index.php');
+    else
+        print 'Error al eliminar un articulo';
+}
+
 function subirimagen() {
 
     $carpeta = __DIR__.'../assets/img_articulos/';
@@ -50,6 +99,4 @@ function subirimagen() {
     move_uploaded_file($_FILES['imagen']['tmp_name'],$archivo);
 
     return $_FILES['imagen']['name'];
-
-
 }
